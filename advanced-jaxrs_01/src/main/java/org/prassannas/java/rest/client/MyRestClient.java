e93f4e4874dcac5f4027645d8f8868d2f2a4a476
SPrassanna <sprassanna@ceiamerica.com>
2016-05-18 19:29:32 +0530

    Updated  my rest client

---
+++ advanced-jaxrs_01/src/main/java/org/prassannas/java/rest/client/MyRestClient.java
@@ -0,0 +1,20 @@
+package org.prassannas.java.rest.client;
+
+import java.util.Date;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.sun.security.ntlm.Client;
+
+public class MyRestClient {
+
+	public static void main(String[] args) {
+		javax.ws.rs.client.Client client = ClientBuilder.newClient();
+		
+		Response response = client.target("http://localhost:8080/advanced-jaxrs_01/root/today").request().get();
+		System.out.println(response);
+	}
+
+}
