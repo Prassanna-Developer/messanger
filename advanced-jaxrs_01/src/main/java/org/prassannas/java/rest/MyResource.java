5606068f77b12d364fe044f96d7b9192236a5a51
SPrassanna <sprassanna@ceiamerica.com>
2016-05-18 18:44:14 +0530

    removed unused imports

--- advanced-jaxrs_01/src/main/java/org/prassannas/java/rest/MyResource.java
+++ advanced-jaxrs_01/src/main/java/org/prassannas/java/rest/MyResource.java
@@ -1,6 +1,5 @@
 package org.prassannas.java.rest;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
--- advanced-jaxrs_01/src/main/java/org/prassannas/java/rest/MyResource1.java
+++ advanced-jaxrs_01/src/main/java/org/prassannas/java/rest/MyResource1.java
@@ -1,6 +1,5 @@
 package org.prassannas.java.rest;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,16 +7,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-
 @Path("{pathParam}/newtest")
 public class MyResource1 {
-	
-	@PathParam("pathParam") private String pathParamExample;
-	@QueryParam("query") private String queryParamExample;
-	
+
+	@PathParam("pathParam")
+	private String pathParamExample;
+	@QueryParam("query")
+	private String queryParamExample;
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getTest(){
+	public String getTest() {
 		return "TEST  >> " + pathParamExample + "..." + queryParamExample;
 	}
 }
